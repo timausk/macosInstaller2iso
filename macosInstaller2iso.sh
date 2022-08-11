a0fce3ecb272007fb55a84fed9890b11b5f91e90
timausk <timausk@googlemail.com>
2022-08-11 16:36:08 +0200

    add support for monterey

--- macosInstaller2iso.sh
+++ macosInstaller2iso.sh
@@ -5,6 +5,7 @@
 # Creates a bootable ISO for VirtualBox from a
 # Mac App Store macOS installer
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+echo "Creates a bootable ISO from macOS installer"
 
 # Ask for the administrator password upfront.
 sudo -v &> /dev/null
@@ -14,7 +15,36 @@ while true; do
     kill -0 "$$" || exit
 done &> /dev/null &
 
-echo "Create a bootable ISO from a App Store macOS installer"
+osVersion=false
+PS3="Choose a macOS Version (Q Quit) "
+options=("Monterey" "High Sierra")
+select opt in "${options[@]}"; do
+    case "$REPLY" in
+    1) osVersion="${opt}"; break;;
+    2) osVersion="${opt}"; break;;
+    Q) echo "Goodbye!"; break;;
+    *) echo "Invalid option. Try another one.";continue;;
+    esac
+done
+
+if [ "$osVersion" == false ]; then
+    exit;
+fi
+
+echo "You choose $osVersion"
+
+case $osVersion in
+  "Monterey")
+    flashDriveSize=14G
+    ;;
+
+  "High Sierra")
+    flashDriveSize=8G
+    ;;
+  *)
+    flashDriveSize=10G
+    ;;
+esac
 
 # options
 echo "press enter to use defaults"
@@ -31,28 +61,29 @@ fi
 
 echo "Generate bootable image";
 
-# create virtual USB flash drive
-hdiutil create -o /tmp/HighSierra -size 8G -layout SPUD -fs HFS+J -type SPARSE
-
-# moaunt flash drive
-hdiutil attach /tmp/HighSierra.sparseimage -noverify -mountpoint /Volumes/install_build
-
 # check if installer exist in Application folder
-if [ ! -d /Applications/Install\ macOS\ High\ Sierra.app ]; then
+if [ ! -d /Applications/Install\ macOS\ "$osVersion".app ]; then
     echo "MacOS Installer not found in Application folder. Download Installer via App Store and try again"
     exit;
 fi
+
+# create virtual USB flash drive
+hdiutil create -o /tmp/"$osVersion" -size "$flashDriveSize" -layout SPUD -fs HFS+J -type SPARSE
+
+# mount flash drive
+hdiutil attach /tmp/"$osVersion".sparseimage -noverify -mountpoint /Volumes/install_build
+
 # put installer file into flash drive
-sudo /Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/install_build
+sudo /Applications/Install\ macOS\ "$osVersion".app/Contents/Resources/createinstallmedia --volume /Volumes/install_build
 
 # unmount
-hdiutil detach /Volumes/Install\ macOS\ High\ Sierra/
+hdiutil detach /Volumes/Install\ macOS\ "$osVersion"/
 
 # convert into iso file
-hdiutil convert /tmp/HighSierra.sparseimage -format UDTO -o /tmp/HighSierra.iso
+hdiutil convert /tmp/"$osVersion".sparseimage -format UDTO -o /tmp/"$osVersion".iso
 
 # mv to desktop folder
-mv /tmp/HighSierra.iso.cdr ~/Desktop/HighSierra.iso
+mv /tmp/"$osVersion".iso.cdr ~/Desktop/"$osVersion".iso
 
 # delete sparseimage
-rm /tmp/HighSierra.sparseimage
+rm /tmp/"$osVersion".sparseimage
